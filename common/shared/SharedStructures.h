4e0ad21ab1e4e631872a18680cfaf2565b8cf2e8
maris <marisusisme@gmail.com>
2021-12-20 16:29:47 -0500

    added shared structures

---
+++ common/shared/SharedStructures.h
@@ -0,0 +1,17 @@
+//
+// Created by maris on 12/20/2021.
+//
+
+#ifndef ROUTE_SUITE_SHAREDSTRUCTURES_H
+#define ROUTE_SUITE_SHAREDSTRUCTURES_H
+
+namespace Route {
+
+    struct route_info {
+        char[256] name;
+        int version;
+    };
+
+}
+
+#endif //ROUTE_SUITE_SHAREDSTRUCTURES_H
